fc08e680d175536f2a1e8a6c2f3e12ce175ba562
Haruka TAKAHASHI <laura.bodewig.marina@gmail.com>
2016-03-21 01:22:27 +0900

    proto

--- server.py
+++ server.py
@@ -9,7 +9,26 @@ import tornado.escape
 import tornado.options
 from tornado.options import define, options
 import logging
+import sqlite3
+from datetime import date,datetime
+from PIL import Image
 
+
+conn = sqlite3.connect("proconDB.db")
+c = conn.cursor()
+if list(c.execute("select count(*) from sqlite_master where type='table' and name='blog';"))[0][0] == 0:
+  b_d = u"""
+  create table blog(
+    id integer primary key,
+    writer varchar(10),
+    date datetime,
+    title varchar(30),
+    category varchar(12),
+    body varchar(300)
+        );
+  """
+  c.execute(b_d)
+  conn.commit()
 
 define("port", default=5000, type=int)
 define("username", default="user")
@@ -37,39 +56,77 @@ class MainHandler(BaseHandler):
 
 
 class AuthLoginHandler(BaseHandler):
-	def get(self):
-		self.render("login.html")
+  def get(self):
+    self.render("login.html")
 
-	def post(self):
-		logging.debug("xsrf_cookie:" + self.get_argument("_xsrf", None))
+  def post(self):
+    logging.debug("xsrf_cookie:" + self.get_argument("_xsrf", None))
+    
+    self.check_xsrf_cookie()
 
-		self.check_xsrf_cookie()
+    username = self.get_argument("username")
+    password = self.get_argument("password")
 
-		username = self.get_argument("username")
-		password = self.get_argument("password")
+    logging.debug('AuthLoginHandler:post %s %s' % (username, password))
 
-		logging.debug('AuthLoginHandler:post %s %s' % (username, password))
-
-		if username == options.username and password == options.password:
-			self.set_current_user(username)
-			self.redirect("/")
-		else:
+    user_data=list(c.execute('''SELECT * FROM admin where name="%s";''' % username))[0]
+    print(user_data)
+    if user_data != None and user_data[2] == password:
+      self.set_current_user(username)
+      self.redirect("/home")
+    else:
 			self.write_error(403)
 
 class AuthLogoutHandler(BaseHandler):
 	def get(self):	
 		self.clear_current_user()
-		self.redirect('/')
+		self.redirect('/home')
 
-class HomeHandler(tornado.web.RequestHandler):
+class HomeHandler(BaseHandler):
   def get(self):
-  	self.render("home.html")
+  	self.render("home.html",
+                login_user = self.get_current_user()
+                )
 class ActHandler(tornado.web.RequestHandler):
   def get(self):
   	self.render("activity.html")
-class BlogHandler(tornado.web.RequestHandler):
+
+class BlogHandler(BaseHandler):
   def get(self):
-  	self.render("blog.html")
+    blog_data=c.execute("SELECT * FROM blog ORDER BY id DESC;")
+
+    self.render("blog.html",
+                blog_data = blog_data,
+                login_user = self.get_current_user()
+                )
+   
+  def post(self):
+    blog_id = list(c.execute("select count(*) from blog;"))[0][0] + 1
+    writer = self.get_current_user()
+    date = datetime.now()
+    title = self.get_argument('blog_title')
+    category = self.get_argument('blog_category')
+    body = self.get_argument('blog_body')
+    print(body)
+    
+    c.execute('''insert into blog(id,writer,date,title,category,body) values(:id, :writer, :date, :title, :category, :body)''', {'id':blog_id, 'writer':writer, 'date': date.today(), 'title':title, 'category':category, 'body':body})
+    conn.commit()
+    
+    self.redirect("/blog")
+               
+               
+class ImageSaveHandler(BaseHandler):
+  def post(self):
+		file1 = self.request.files['aaa'][0]
+		original_fname = file1['filename']
+		extension = os.path.splitext(original_fname)[1]
+		fname=str(datetime.now())[:19]+"_"+self.get_current_user()
+		final_filename= fname+extension
+		output_file = open("img/" + final_filename, 'w')
+		output_file.write(file1['body'])
+		self.redirect("/blog")
+
+
 class LinkHandler(tornado.web.RequestHandler):
   def get(self):
     self.render("link.html")
@@ -83,6 +140,7 @@ class Application(tornado.web.Application):
       (r"/home", HomeHandler),
       (r"/activity", ActHandler),
       (r"/blog", BlogHandler),
+      (r"/blog/image_save", ImageSaveHandler),
       (r"/link", LinkHandler),
       (r"/wiki", WikiHandler),
 		  (r'/', MainHandler),
